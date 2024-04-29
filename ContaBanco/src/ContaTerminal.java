26ddee10fec8ce0236b3eb88bb25f5b7ca81d443
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2024-04-29 15:26:18 -0300

    :wrench:	chore criada a funcionalidade
    :wrench:	chore criada a funcionalidade

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Digite o número da sua Conta");
+        int conta = scaner.nextInt();
+
+        scaner.nextLine();
+
+        System.out.println("Digite a sua Agencia");
+        String numAgencia = scaner.nextLine();
+
+        System.out.println("Digite Seu nome Completo");
+        String nomeCompleto = scaner.nextLine();
+
+        System.out.println("Digite o valor do seu Saldo");
+        double saldo = scaner.nextDouble();
+
+        System.out.println("Olá " + nomeCompleto + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + numAgencia + ", conta " + conta + "e seu saldo " + saldo + " já está disponivel para saque."  );
+
+
+
+
+
     }
 }
